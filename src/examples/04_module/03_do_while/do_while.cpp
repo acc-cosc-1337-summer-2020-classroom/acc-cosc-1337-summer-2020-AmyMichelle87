9dc12ccbee070a1f0a8f6f9b57ce2674d8337101
amyMichelle87 <amy.hartmann@g.austincc.edu>
2020-06-11 19:06:47 -0500

    do while loop examples

--- src/examples/04_module/03_do_while/do_while.cpp
+++ src/examples/04_module/03_do_while/do_while.cpp
@@ -3,5 +3,12 @@
 //Write code for void function prompt_user to loop until
 //user opts not to continue.  
 
+void prompt_user(){
+    char choice; 
 
-
+    do{
+        std::cout<<"Enter y or n to continue: ";
+        std::cin>>choice; 
+    }
+    while(choice == 'y' || choice == 'Y');
+}
--- src/examples/04_module/03_do_while/main.cpp
+++ src/examples/04_module/03_do_while/main.cpp
@@ -1,6 +1,7 @@
 
 int main() 
 {	
+	prompt_user();
 	
 	return 0;
 }
