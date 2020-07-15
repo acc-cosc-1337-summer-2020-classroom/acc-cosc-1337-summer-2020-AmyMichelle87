e45636c7557de853eef07c0043252d9b14973a91
amyMichelle87 <amy.hartmann@g.austincc.edu>
2020-07-15 08:48:40 -0500

    mod 9 example continued

--- src/examples/09_module/01_ref_pointers/main.cpp
+++ src/examples/09_module/01_ref_pointers/main.cpp
@@ -1,15 +1,10 @@
 #include "ref_pointers.h"
 #include<iostream>
-using std::cout; 
+
 
 int main() 
 {
-	int r = 0, p = 0; 
-	int& ref_r = r; 
-
-	cout<<"Address of r: "<<&r; 
-	cout<<"Value of r: " << r; 
-	cout<<"Address of ref_r: "<<&ref_r;
+	
 
 	return 0;
 }
